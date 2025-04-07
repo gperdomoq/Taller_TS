a3beb9cd1dd04c6fd080185d2fbcb68c581da0b1
Gabriela <g.perdomoq@uniandes.edu.co>
2025-04-06 21:17:15 -0500

    Taller 1

--- Taller 1/data.ts
+++ Taller 1/data.ts
@@ -1,3 +1,15 @@
+export class Serie {
+    constructor(
+      public id: number,
+      public name: string,
+      public channel: string,
+      public seasons: number,
+      public description: string,
+      public url: string,
+      public image: string
+    ) {}
+  }
+
 export const series = [
     new Serie (1,"Breaking Bad","AMC", 5,"Set and filmed in Albuquerque, New Mexico, the series tells the story of Walter White, a struggling and depressed high school chemistry teacher who is diagnosed with lung cancer" ,
     "https://www.amc.com/shows/breaking-bad","https://i.imgur.com/GGje0vc.jpg") ,
